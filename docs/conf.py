b40359e72ff997bff6e815f3d1a318dd1a3f8e41
Xiangchong <xiangchong.li@ipmu.jp>
2021-12-02 00:38:53 -0500

    fix readthedocs module problem

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,7 @@ release = '2.0.1'
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
+autodoc_mock_imports = ["numpy", "scipy", "astropy"]
 
 # -- General configuration ---------------------------------------------------
 
