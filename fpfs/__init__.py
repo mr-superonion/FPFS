2130a28f17b75f9d22a43008251d0bd3a33ce8ef
Xiangchong Li <xiangchong.li@ipmu.jp>
2022-03-29 15:37:46 -0400

    2.0.3

--- fpfs/__init__.py
+++ fpfs/__init__.py
@@ -2,6 +2,6 @@ from . import fpfsBase
 from . import imgutil
 from . import simutil
 
-__version__="2.0.2"
+__version__="2.0.3"
 
 __all__= ["fpfsBase", "imgutil", "simutil"]
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fpfs',
-    version='2.0.2',
+    version='2.0.3',
     description='FPFS shear estimator',
     author='Xiangchong Li et al.',
     author_email='mr.superonion@hotmail.com',
