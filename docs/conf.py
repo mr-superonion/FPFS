7fea26df0231169d9a1436f1a7fc95377bd74753
Xiangchong Li <mr.superonion@hotmail.com>
2022-04-27 06:47:06 +0900

    add readthedocs yaml

--- docs/conf.py
+++ docs/conf.py
@@ -10,11 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../fpfs'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('../fpfs'))
 
-
+import fpfs
 # -- Project information -----------------------------------------------------
 
 project = 'FPFS'
@@ -22,7 +22,7 @@ copyright = '2022, Xiangchong Li'
 author = 'Xiangchong Li'
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.3"
+release = fpfs.__version__
 
 # -- General configuration ---------------------------------------------------
 
