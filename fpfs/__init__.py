dfe2db285612695b8f97bfc5f34ffb6eea3d1b6b
Xiangchong Li <xiangchong.li@ipmu.jp>
2022-05-04 13:04:26 -0400

    no numpy in setup.py

--- fpfs/__init__.py
+++ fpfs/__init__.py
@@ -2,6 +2,6 @@ from . import fpfsBase
 from . import imgutil
 from . import simutil
 
-__version__="2.0.4"
+__version__="2.0.5"
 
 __all__= ["fpfsBase", "imgutil", "simutil"]
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fpfs',
-    version='2.0.4',
+    version='2.0.5',
     description='FPFS shear estimator',
     author='Xiangchong Li et al.',
     author_email='mr.superonion@hotmail.com',
