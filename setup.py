bdd96baf5a5e9affd177a468cc4039234b5e94d9
Xiangchong Li <mr.superonion@hotmail.com>
2023-01-10 02:10:34 +0900

    lint

--- setup.py
+++ setup.py
@@ -2,42 +2,40 @@ import os
 from setuptools import setup, find_packages
 
 # version of the package
-__version__ = ''
+__version__ = ""
 fname = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    "fpfs",
-    "__version__.py")
-with open(fname, 'r') as ff:
+    os.path.dirname(os.path.realpath(__file__)), "fpfs", "__version__.py"
+)
+with open(fname, "r") as ff:
     exec(ff.read())
 
 
-
 scripts = [
-    'bin/fpfs_sim.py',
-    'bin/fpfs_procsim.py',
-    'bin/fpfs_summary.py',
-    'bin/setup_fpfs',
+    "bin/fpfs_sim.py",
+    "bin/fpfs_procsim.py",
+    "bin/fpfs_summary.py",
+    "bin/setup_fpfs",
 ]
 
 setup(
-    name='fpfs',
+    name="fpfs",
     version=__version__,
-    description='FPFS shear estimator',
-    author='Xiangchong Li',
-    author_email='mr.superonion@hotmail.com',
-    python_requires='>=3.6',
+    description="FPFS shear estimator",
+    author="Xiangchong Li",
+    author_email="mr.superonion@hotmail.com",
+    python_requires=">=3.6",
     install_requires=[
-        'numpy<1.23',
-        'numba',
-        'scipy',
-        'schwimmbad',
-        'galsim',
-        'astropy',
-        'matplotlib',
+        "numpy<1.23",
+        "numba",
+        "scipy",
+        "schwimmbad",
+        "galsim",
+        "astropy",
+        "matplotlib",
     ],
     packages=find_packages(),
     scripts=scripts,
     include_package_data=True,
     zip_safe=False,
-    url = "https://github.com/mr-superonion/FPFS/",
+    url="https://github.com/mr-superonion/FPFS/",
 )
