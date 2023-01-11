b6ae804724483666bb3edcdb544db2de91e96909
Xiangchong Li <mr.superonion@hotmail.com>
2023-01-11 10:43:56 +0900

    commit

--- docs/conf.py
+++ docs/conf.py
@@ -113,8 +113,8 @@ html_theme_options = {
     },
 }
 
-def setup(app):
-    app.add_css_file('xtheme.css')
+# def setup(app):
+#     app.add_css_file('xtheme.css')
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
