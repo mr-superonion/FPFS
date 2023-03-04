a9194b1f8e34a672bf96afdf93ce1d8bb0ffd883
Xiangchong Li <mr.superonion@hotmail.com>
2023-03-05 04:46:25 +0900

    put notebooks not in examples to another repo for notebooks

--- notebooks/paper_2022/3_test_2/1_isolated_force/run.sh
+++
@@ -1,7 +0,0 @@
-myqsubMini mpirun -np 52 ./meas_center_r2.py --minId 0 --maxId 3000 &&
-sleep 1 &&
-myqsubMini mpirun -np 52 ./meas_center_mag.py --minId 0 --maxId 3000 &&
-sleep 1 &&
-myqsubMini mpirun -np 52 ./meas_constC.py --minId 0 --maxId 3000 --noirev &&
-sleep 1 &&
-myqsubMini mpirun -np 52 ./meas_constC.py --minId 0 --maxId 3000 --no-noirev
