9e6d494b65ce3485c505ce7bf80dd89ba6cbb07f
Xiangchong Li <mr.superonion@hotmail.com>
2023-08-10 15:18:43 -0400

    add pyarrow based io

--- fpfs/__init__.py
+++ fpfs/__init__.py
@@ -1,5 +1,6 @@
 # flake8: noqa
 from .__version__ import __version__
+from . import io
 from . import image
 from . import imgutil
 from . import catalog
@@ -9,4 +10,4 @@ from . import default
 from . import pltutil
 from .default import __data_dir__
 
-__all__ = ["image", "imgutil", "catalog", "simutil", "pltutil", "default"]
+__all__ = ["image", "imgutil", "catalog", "simutil", "pltutil", "default", "io"]
