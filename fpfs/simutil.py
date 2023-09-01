37f5f0f8633f523b7b71c77eff254f37ade9aff7
Xiangchong Li <mr.superonion@hotmail.com>
2023-09-01 15:07:50 -0400

    add nonaffine trans

--- fpfs/simutil.py
+++ fpfs/simutil.py
@@ -810,3 +810,70 @@ def make_gal_ssbg(shear, psf, rng, r1, r0=20.0):
     noimg = noimg * np.sqrt(back_flux)
     img = img + noimg
     return img
+
+
+class Stamp(object):
+    def __init__(self, coords=None, nn=32, scale=0.2):
+        """Initialize the 2D stamp object. This class enables distorting
+        an image by changing the samplinng position with non-affine
+        transformation
+
+        Args:
+            nn (int):      number of grids on x and y direction
+            scale (float): pixel scale in units of arcsec
+        """
+        if coords is None:
+            indx = np.arange(-int(nn / 2), int((nn + 1) / 2), 1) * scale
+            indy = np.arange(-int(nn / 2), int((nn + 1) / 2), 1) * scale
+            inds = np.meshgrid(indy, indx, indexing="ij")
+            self.coords = np.vstack([np.ravel(_) for _ in inds[::-1]])
+        else:
+            self.coords = coords
+        self.pixel_values = None
+        self.transformed = False
+        if self.coords.shape[0] != 2:
+            raise ValueError("Something wrong with the coordinate shape")
+        self.shape = (nn, nn)
+        return
+
+    def sample_galaxy(self, gal_obj):
+        """Sample the surface density field of a galaxy at the grids
+        This function only conduct sampling; PSF and pixel response are
+        not included.
+
+        Args:
+            gal_obj (galsim):   Galsim galaxy object to sample on the grids
+        Returns:
+            outcome (ndarray):  2D galaxy image on the grids
+        """
+        pixel_values = np.array([gal_obj.xValue(cc) for cc in self.coords.T])
+
+        return np.reshape(pixel_values, self.shape)
+
+    def transform_grids(self, transform_obj):
+        if not hasattr(transform_obj, "transform"):
+            raise TypeError("transform_obj is not in correct data type")
+        self.coords = transform_obj.transform(self.coords)
+        self.transformed = True
+        return
+
+
+class LensTransform1(object):
+    def __init__(self, gamma1, gamma2, kappa):
+        """Initialize the transform object of 2D grids
+        Args:
+            gamma1 (float):     the first component of lensing shear field
+            gamma2 (float):     the second component of lensing shear field
+            kappa (float):      the lensing convergence field
+        """
+        self.s2l_mat = np.array(
+            [[1 - kappa - gamma1, -gamma2], [-gamma2, 1 - kappa + gamma1]]
+        )
+        return
+
+    def transform(self, coords):
+        """transform the center of pixels from lensed plane to pre-lensed plane
+        Args:
+            coords:   coordinates (x, y) of the pixel centers [arcsec]
+        """
+        return self.s2l_mat @ coords
