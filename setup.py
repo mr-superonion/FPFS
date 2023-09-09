63c7f8f0cf1521cc26b0d37c55f85c8187a4e3d6
Xiangchong Li <mr.superonion@hotmail.com>
2023-09-09 17:32:15 -0400

    add long description on pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import os
 from setuptools import setup, find_packages
 
-# version of the package
+
+this_dir = os.path.dirname(os.path.realpath(__file__))
 __version__ = ""
-fname = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), "fpfs", "__version__.py"
-)
+fname = os.path.join(this_dir, "fpfs", "__version__.py")
 with open(fname, "r") as ff:
     exec(ff.read())
+long_description = open(os.path.join(this_dir, "README.md")).read()
 
 
 scripts = [
@@ -41,4 +41,6 @@ setup(
     include_package_data=True,
     zip_safe=False,
     url="https://github.com/mr-superonion/FPFS/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
